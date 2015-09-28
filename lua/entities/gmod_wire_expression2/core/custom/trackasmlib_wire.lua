8f6389ebb4861f9f0dba23de367ab3185ed81035
dvdvideo1234 <dvd_video@abv.bg>
2015-09-28 17:32:09 +0300

    Added: Wiremod support Updated: readme.md Updated: Tool general names handling Reordered: TOOL:LeftClick()

---
+++ lua/entities/gmod_wire_expression2/core/custom/trackasmlib_wire.lua
@@ -0,0 +1,227 @@
+local asmlib = trackasmlib
+
+--------- Pieces ----------
+
+__e2setcost(100)
+e2function entity entity:trackasmlibGetEntitySpawn(vector trHitPos  , string hdModel  , number hdPointID,
+                                          number nActRadius, number enFlatten, number enIgnTyp ,
+                                          vector ucsPos    , vector ucsAng)
+  if(not IsValid(this)) then return nil end
+	if(not PropCore.ValidAction(self, this, "trackassembly_spawn_entity")) then return nil end
+  local stSpawn = asmlib.GetEntitySpawn(this,trHitPos,hdModel,hdPointID,
+                                        nActRadius,enFlatten,enIgnTyp,
+                                        ucsPos[1],ucsPos[2],ucsPos[3],ucsAng[1],ucsAng[2],ucsAng[3])
+  if(not stSpawn) then return nil end
+	return PropCore.CreateProp(self,hdModel,stSpawn.SPos,stSpawn.SAng,frozen)
+end
+
+__e2setcost(80)
+e2function entity trackasmlibGetNormalSpawn(vector ucsPos, angle ucsAng, string hdModel, number hdPointID, ucsOffPos,ucsOffAng)
+	if not PropCore.ValidAction(self, nil, "trackassembly_spawn_normal") then return nil end
+  local stSpawn = GetNormalSpawn(ucsPos,ucsAng,hdModel,hdPointID,
+                        ucsOffPos[1],ucsOffPos[2],ucsOffPos[3],ucsOffAng[1],ucsOffAng[2],ucsOffAng[3])
+  if(not stSpawn) then return nil end
+	return PropCore.CreateProp(self,hdModel,stSpawn.SPos,stSpawn.SAng,frozen)
+end
+
+__e2setcost(30)
+e2function number trackasmlibExists(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_exist") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(stRecord) then return 1 else return 0 end
+end
+
+__e2setcost(30)
+e2function number entity:trackasmlibExists()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_exist") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(string.lower(this:GetModel()))
+  if(stRecord) then return 1 else return 0 end
+end
+
+__e2setcost(120)
+e2function number trackasmlibGetOffset(string sModel, number nOffset, string sPOA)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_poa") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(not stRecord) then return nil end
+  if(not stRecord.Offs) then return nil end
+  local nOffset = tonumber(nOffset)
+  if(not nOffset) then return nil end
+  if(not stRecord.Offs[nOffset]) then return nil else stRecord = stRecord.Offs[nOffset] end
+  local sPOA = string.sub(string.upper(tostring(sPOA)),1,1)
+  local arResult = {}
+  local C1, C2, C3, C4
+  if(sPOA == "P") then
+    C1, C2, C3 = asmlib.GetIndexes("V")
+  elseif(sPOA == "O") then
+    C1, C2, C3 = asmlib.GetIndexes("V")
+  elseif(sPOA == "A") then
+    C1, C2, C3 = asmlib.GetIndexes("A")
+  else
+    return nil
+  end  
+  arResult[1] = stRecord[sPOA][C1]
+  arResult[2] = stRecord[sPOA][C2]
+  arResult[3] = stRecord[sPOA][C3]
+  C1, C2, C3, C4 = asmlib.GetIndexes("S")
+  arResult[4] = stRecord[sPOA][C1]
+  arResult[5] = stRecord[sPOA][C2]
+  arResult[6] = stRecord[sPOA][C3]
+  arResult[7] = stRecord[sPOA][C4] and 1 or 0
+  return arResult
+end
+
+__e2setcost(120)
+e2function number entity:trackasmlibGetOffset(number nOffset)
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_poa") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(string.lower(this:GetModel()))
+  if(not stRecord) then return nil end
+  if(not stRecord.Offs) then return nil end
+  local nOffset = tonumber(nOffset)
+  if(not nOffset) then return nil end
+  if(not stRecord.Offs[nOffset]) then return nil else stRecord = stRecord.Offs[nOffset] end
+  local sPOA = string.sub(string.upper(tostring(sPOA)),1,1)
+  local arResult = {}
+  local C1, C2, C3, C4
+  if(sPOA == "P") then
+    C1, C2, C3 = asmlib.GetIndexes("V")
+  elseif(sPOA == "O") then
+    C1, C2, C3 = asmlib.GetIndexes("V")
+  elseif(sPOA == "A") then
+    C1, C2, C3 = asmlib.GetIndexes("A")
+  else
+    return nil
+  end  
+  arResult[1] = stRecord[sPOA][C1]
+  arResult[2] = stRecord[sPOA][C2]
+  arResult[3] = stRecord[sPOA][C3]
+  C1, C2, C3, C4 = asmlib.GetIndexes("S")
+  arResult[4] = stRecord[sPOA][C1]
+  arResult[5] = stRecord[sPOA][C2]
+  arResult[6] = stRecord[sPOA][C3]
+  arResult[7] = stRecord[sPOA][C4] and 1 or 0
+  return arResult
+end
+
+__e2setcost(30)
+e2function string trackasmlibGetType(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_type") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(stRecord) then return stRecord.Type else return nil end
+end
+
+__e2setcost(30)
+e2function string entity:trackasmlibGetType()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_type") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(string.lower(this:GetModel()))
+  if(stRecord) then return stRecord.Type else return nil end
+end
+
+__e2setcost(30)
+e2function string trackasmlibGetName(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_name") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(stRecord) then return stRecord.Name else return nil end
+end
+
+__e2setcost(30)
+e2function string entity:trackasmlibGetName()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_name") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(string.lower(this:GetModel()))
+  if(stRecord) then return stRecord.Name else return nil end
+end
+
+__e2setcost(30)
+e2function number trackasmlibGetPointsCount(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_kept") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(stRecord) then return stRecord.Kept else return nil end
+end
+
+__e2setcost(30)
+e2function number entity:trackasmlibGetPointsCount()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_kept") then return nil end
+  local stRecord = asmlib.CacheQueryPiece(sModel)
+  if(stRecord) then return stRecord.Kept else return nil end
+end
+
+---------- Additions ------------
+
+__e2setcost(30)
+e2function number trackasmlibHasAdditions(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_hasadd") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(sModel)
+  if(stRecord) then return 1 else return 0 end
+end
+
+__e2setcost(30)
+e2function number entity:trackasmlibHasAdditions()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_hasadd") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(string.lower(this:GetModel()))
+  if(stRecord) then return 1 else return 0 end
+end
+
+__e2setcost(50)
+e2function number trackasmlibGetAdditionsCount(string sModel)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_addcnt") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(sModel)
+  if(stRecord) then return stRecord.Kept else return nil end
+end
+
+__e2setcost(50)
+e2function number entity:trackasmlibGetAdditionsCount()
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_addcnt") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(string.lower(this:GetModel()))
+  if(stRecord) then return stRecord.Kept else return nil end
+end
+
+__e2setcost(60)
+e2function array trackasmlibGetAdditionsLine(string sModel, number nLine)
+  local nLine = tonumber(nLine)
+  if(not nLine) then return nil end
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_addcnt") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(sModel)
+  if(not (stRecord and stRecord[nLine])) then return nil else stRecord = stRecord[nLine] end
+  local arAddtitionsLine = {}
+  local defTable = asmlib.GetOpVar("TABLEDEF_ADDITIONS")
+  local cntField = 2
+  while(defTable[cntField]) do
+    arAddtitionsLine[cntField] = stRecord[defTable[cntField][1]]
+    cntField = cntField + 1
+  end
+  return arAddtitionsLine
+end
+
+__e2setcost(60)
+e2function array entity:trackasmlibGetAdditionsLine(number nLine)
+  local nLine = tonumber(nLine)
+  if(not nLine) then return nil end
+	if not PropCore.ValidAction(self, this, "trackassembly_piece_addcnt") then return nil end
+  local stRecord = asmlib.CacheQueryAdditions(string.lower(this:GetModel()))
+  if(not (stRecord and stRecord[nLine])) then return nil else stRecord = stRecord[nLine] end
+  local arAddtitionsLine = {}
+  local defTable = asmlib.GetOpVar("TABLEDEF_ADDITIONS")
+  local cntField = 2
+  while(defTable[cntField]) do
+    arAddtitionsLine[cntField] = stRecord[defTable[cntField][1]]
+    cntField = cntField + 1
+  end
+  return arAddtitionsLine
+end
+
+------------ PhysProperties ------------
+
+__e2setcost(15)
+e2function array trackasmlibGetProperty(string sType)
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_proptype") then return nil end
+  local stRecord = asmlib.CacheQueryProperty(sType)
+  if(not stRecord) then return nil end
+	return stRecord
+end
+
+__e2setcost(15)
+e2function array trackasmlibGetProperty()
+	if not PropCore.ValidAction(self, nil, "trackassembly_piece_property") then return nil end
+  local stRecord = asmlib.CacheQueryProperty()
+  if(not stRecord) then return nil end
+	return stRecord
+end
--- readme.md
+++ readme.md
@@ -64,7 +64,7 @@ A: Here they are, with available status, why I did not do some of them ( in the 
    15) Some Bridges
    16) gm_sunsetgulch map props ( Ignore, it's not designed to be a prop )
    17) StevenTechno's Buildings pack
-   18) Mr. Train's M-Gauge rails ( Done 45% )
+   18) Mr. Train's M-Gauge rails
 
 Q: Are there going to be more of those?
 A: Yes, I developed my dynamic database, so I can insert any model I want.
@@ -80,7 +80,7 @@ Q: How can I make a track with multiple piece segments?
 A: Hold Speed/Run ( Default: Shift ), point to an active position then Left click while holding.
 
 Q: How to choose Previous, Next active position and vice-versa?
-A: Shift+RightClick, Right Click.
+A: Shift + RightClick, Right Click.
 
 Q: Can I assemble tracks in a different than default way?
 A: Yes, use the X,Y,Z P,Y and R offsets.
@@ -111,13 +111,13 @@ A: Check "Next piece flat to surface" track option, then point
 Q: I want to place the first piece relative to the mass-centre like the previous
    version or relative to the chosen point, how can I do that ?
 A: EEmm, check/uncheck "Spawn at mass-centre" checkbox.
-Note: If you uncheck it, the position were you pointing at becomes origin,
+N: If you uncheck it, the position were you pointing at becomes origin,
    so you need to offset your piece as you want to, using the Pos/Ang offsets!
 
 Q: I want to snap the first piece in N-Degrees. Please Help ! ( Requested by Magnum )
 A: See the "Yaw snap amount" slider ! Use that to Snap your first piece.
    Everything greater than 0 will become the "Snap amount".
-Note: Assembling the track relative to the first piece will be snapped also, because
+N: Assembling the track relative to the first piece will be snapped also, because
    you are building it relative to the first piece as an origin !
 
 Q: Are there any other options?
@@ -154,7 +154,7 @@ A: Easy, :D Just set "trackassembly_bnderrmod" to one of the following values
    But remember young samurai, this variable is only server side, and because of that
    you can only access it via single player or set it in the "server.vdf" gmod start-up file. 
    May the force be with you and your server !
-Note: The error is logged if the logs are enabled !
+N: The error is logged if the logs are enabled !
    ( See:  Hay, how should I proceed when I am experiencing errors points 10 - 12 ).
 
 Q: May I PUT this thing in another third party website ?
@@ -166,43 +166,43 @@ A: No I will not give you my permission to do that... Why ...
 
 Q: Hay, how should I proceed when I am experiencing errors ?
 A: First of all if the the error origin is not the TA, 
-   I can't pretty much help you with it, but I will do my best
-   If the error is related to the TA then:
-   1) While ingame ( or the TA's link ) unsubscribe to TA
-   2) Delete the TA's *.gma file
-   3) Enter Gmod to see that you've unsubscribed TA
-   4) Delete the cache "..\GarrysMod\garrysmod\cache"
-   5) In the game library, right click on Gmod and select "Properties"
-   6) Navigate to "Local Files" and click "Verify integrity of the game cache"
-   7) Go to http://steamcommunity.com/sharedfiles/filedetails/?id=287012681 
-   8) Hit the subscribe button
-   9) If the error does not go away pls proceed to ( 10 - 16 )
-  10) Enable the logs via the console "trackassembly_logsmax 10000" hit enter
-  11) Enable the log file via the console "trackassembly_logfile trackasmlib_log" hit enter
-  12) Point to the world ingame hold "IN_SPEED" ( Running: Shift ) + Reload
-  13) Now the hardest part - While ingame do some stuff and make it crash 
-  14) Navigate to ..Steam\steamapps\common\GarrysMod\garrysmod\data\trackassembly\trackasmlib_log.txt
-  15) Report the log and the error to http://steamcommunity.com/workshop/filedetails/discussion/287012681/35220315967153021/
-  16) If you don't boter using the workshop, then please use the GitHub link instead
-      GitHub: https://github.com/dvdvideo1234/TrackAssemblyTool/tree/master
-      SVN     https://github.com/dvdvideo1234/TrackAssemblyTool/trunk
+    I can't pretty much help you with it, but I will do my best
+    If the error is related to the TA then:
+    1) While ingame ( or the TA's link ) unsubscribe to TA
+    2) Delete the TA's *.gma file
+    3) Enter Gmod to see that you've unsubscribed TA
+    4) Delete the cache "..\GarrysMod\garrysmod\cache"
+    5) In the game library, right click on Gmod and select "Properties"
+    6) Navigate to "Local Files" and click "Verify integrity of the game cache"
+    7) Go to http://steamcommunity.com/sharedfiles/filedetails/?id=287012681 
+    8) Hit the subscribe button
+    9) If the error does not go away pls proceed to ( 10 - 16 )
+   10) Enable the logs via the console "trackassembly_logsmax 10000" hit enter
+   11) Enable the log file via the console "trackassembly_logfile trackasmlib_log" hit enter
+   12) Point to the world ingame hold "IN_SPEED" ( Running: Shift ) + Reload
+   13) Now the hardest part - While ingame do some stuff and make it crash 
+   14) Navigate to ..Steam\steamapps\common\GarrysMod\garrysmod\data\trackassembly\trackasmlib_log.txt
+   15) Report the log and the error to http://steamcommunity.com/workshop/filedetails/discussion/287012681/35220315967153021/
+   16) If you don't boter using the workshop, then please use the GitHub link instead
+       GitHub: https://github.com/dvdvideo1234/TrackAssemblyTool/tree/master
+       SVN     https://github.com/dvdvideo1234/TrackAssemblyTool/trunk
 
 Q: Yo, can I add some personal models to TA ?
 A: Yes, you can. For every active point, you have to add a line in the table PIECES.
-   1) In the console ( Bring it up with ~ key under ESC ): "trackassembly_exportdb 1" [ press enter ]
-   2) After exporting, tables are located under ..common\GarrysMod\garrysmod\data\trackassembly\dsvbase\ [DB Folder]
-   3) Server: Point the crosshair anywhere on the map, then hit IN_SPEED ( Def: Shift ) + IN_RELOAD ( Def: R )
-   4) Client: Just bring up the Frequently used pieces screen, then click the "Export client's DB" button
-   5) Use Excel or another table editing program to edit the files sv_*.txt and cl_*.txt
-   6) After you are done, proseed as below [Excel 2010] 
-   6) [Excel 2010] File -> Save As -> Navigate to the DB folder if you are not in there already
-   7) [Excel 2010] File name: *TRACKASSEMBLY_PIECES.txt
-   8) [Excel 2010] Save as type: "Text (Tab delimited)(*.txt)"
-   9) [Excel 2010] Replace it if you must (don't worry you can alway generate it again ( points 3) and 4) )
-  10) [Excel 2010] It will prompt you that the file you are saving does contain features not compatible with "TAB Delimited" format
-  11) [Excel 2010] Just Click "Yes" and close Excel
-  12) [Excel 2010] It will want to save it again, so you should click "Don't Save"
-  13) [Excel 2010] You are good to go
+    1) In the console ( Bring it up with ~ key under ESC ): "trackassembly_exportdb 1" [ press enter ]
+    2) After exporting, tables are located under ..common\GarrysMod\garrysmod\data\trackassembly\dsvbase\ [DB Folder]
+    3) Server: Point the crosshair anywhere on the map, then hit IN_SPEED ( Def: Shift ) + IN_RELOAD ( Def: R )
+    4) Client: Just bring up the Frequently used pieces screen, then click the "Export client's DB" button
+    5) Use Excel or another table editing program to edit the files sv_*.txt and cl_*.txt
+    6) After you are done, proseed as below [Excel 2010] 
+    6) [Excel 2010] File -> Save As -> Navigate to the DB folder if you are not in there already
+    7) [Excel 2010] File name: *TRACKASSEMBLY_PIECES.txt
+    8) [Excel 2010] Save as type: "Text (Tab delimited)(*.txt)"
+    9) [Excel 2010] Replace it if you must (don't worry you can alway generate it again ( points 3) and 4) )
+   10) [Excel 2010] It will prompt you that the file you are saving does contain features not compatible with "TAB Delimited" format
+   11) [Excel 2010] Just Click "Yes" and close Excel
+   12) [Excel 2010] It will want to save it again, so you should click "Don't Save"
+   13) [Excel 2010] You are good to go
 ```
 Just CLK the subscribe to the button above. And also Thumbs Up ! XD
 
