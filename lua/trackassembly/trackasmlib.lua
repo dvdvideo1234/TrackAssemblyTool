ea01f90a20c3edb54c18403681d1827535a9d980
dvdvideo1234 <dvd_video@abv.bg>
2015-10-01 17:27:25 +0300

    Make ModelToName not crash when got different input args

--- lua/trackassembly/trackasmlib.lua
+++ lua/trackassembly/trackasmlib.lua
@@ -909,9 +909,11 @@ function ModelToName(sModel)
   local Cnt = 1
   local sModel = string.sub(sModel,1,Len)
   local tMarks = GsubModel()
-  while(tMarks[Cnt]) do
-    sModel = string.gsub(sModel,tMarks[Cnt],tMarks[Cnt+1])
-    Cnt = Cnt + 2
+  if(tMarks and tMarks[1] and (ArrayCount(tMarks)%2 == 0)) then
+    while(tMarks[Cnt]) do
+      sModel = string.gsub(sModel,tMarks[Cnt],tMarks[Cnt+1])
+      Cnt = Cnt + 2
+    end
   end
   local Fch = ""
   Cnt = Len
