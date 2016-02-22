9ded4618aa28f8b02ae6a261b91209d540e50605
dvdvideo1234 <dvd_video@abv.bg>
2016-02-22 17:01:32 +0200

    Fixed: Flag control management when add-on is in the last place Added: Pieces manager error handling Added: Pieces manager requite once headed Changed: Pieces manager usage of name spaces

--- peaces_manager/main.cpp
+++ peaces_manager/main.cpp
@@ -6,7 +6,30 @@
 #define BUFF_LEN 100000
 #define PATH_LEN 1000
 
-using namespace std;
+// For example <"models/props/something.mdl">
+// <"models/> OR what the model in the line starts with
+#define MATCH_MODEL_STR "\"models/"
+// <.mdl"> OR what the model in the line ends with
+#define MATCH_MODEL_END ".mdl\""
+
+// <asmlib.DefaultType("PHX Metal")>
+// A new track type starts from this line ( e.g. "PHX Metal" )
+#define MATCH_START_NEW_TYPE "asmlib.DefaultType(\""
+
+
+using namespace sstack;
+
+FILE *I, *D, *DA, *AD;
+
+int mainExit(int errID, const char * const errFormat)
+{
+  if(errID < 0){ printf(errFormat,sstack::sstackErrMsg(errID)); }
+  fclose(I);
+  fclose(D);
+  fclose(DA);
+  fclose(AD);
+  return 0;
+}
 
 char *pathToGmod(char *strData)
 {
@@ -24,22 +47,25 @@ char *pathToGmod(char *strData)
 
 int main(int argc, char **argv)
 {
-  cstrStack Addon, DataBase;
-  int Cnt, Len, F; char *S, *E, *F1, *F2;
+  sstack::cstrStack Addon, DataBase;
+  unsigned char F;
+  int Cnt, Len, Err;
+  char *S, *E, *F1, *F2, *F3;
   char basePath[PATH_LEN]  = {0};
   char resuPath[PATH_LEN]  = {0};
   char fName[PATH_LEN]     = {0};
 
+  printf("\nargc: <%d>",argc);
   for(Cnt = 0; Cnt < argc; Cnt++)
   {
     printf("\nargv[%d] = <%s>",Cnt, argv[Cnt]);
   }
 
-  if(argc < 3)
+  if(argc < 5)
   {
     printf("\nToo few parameters.\n");
-    printf("Call with /base_path/, /model_list/, /db_file/ /rail_type/! \n");
-    return 0;
+    printf("Call with /base_path/, /model_list/, /db_file/ /add-on_type/! \n");
+    return mainExit(0,"");
   }
 
   printf("\n");
@@ -51,43 +77,40 @@ int main(int argc, char **argv)
   strcpy(fName,basePath);
   strcat(fName,argv[2]);
 
-  FILE *I = fopen(fName  ,"rt");
-  FILE *D = fopen(argv[3],"rt");
+  I = fopen(fName  ,"rt");
+  D = fopen(argv[3],"rt");
 
   strcpy(fName,basePath);
   strcpy(fName,"db-addon.txt");
-  FILE *DA = fopen(fName,"wt");
+  DA = fopen(fName,"wt");
 
   strcpy(fName,basePath);
   strcpy(fName,"addon-db.txt");
-  FILE *AD = fopen(fName,"wt");
+  AD = fopen(fName,"wt");
 
   if(NULL == I)
   {
     printf("Cannot open input file");
-    return 0;
+    return mainExit(0,"");
   }
 
   if(NULL == D)
   {
     printf("Cannot open database file");
-    return 0;
+    return mainExit(0,"");
   }
 
   if(NULL == DA)
   {
-    printf("Cannot open db-addon");
-    return 0;
+    printf("Cannot open db-addon using stdout");
+    DA = stdout;
   }
 
   if(NULL == AD)
   {
     printf("Cannot open addon-db");
-    return 0;
+    AD = stdout;
   }
-
-  fprintf(AD,"%s\n\n","These models were removed by the extension creator/owner.");
-  fprintf(DA,"%s\n\n","These models are missing in the database. Insert them.");
 
   Cnt = strlen(basePath);
   while(fgets(resuPath,PATH_LEN,I))
@@ -95,24 +118,26 @@ int main(int argc, char **argv)
     strcpy(resuPath,&(resuPath[Cnt]));
     pathToGmod(resuPath);
     resuPath[strlen(resuPath)-1] = '\0';
-    Addon.putString(resuPath);
+    Err = Addon.putString(resuPath);
+    if(Err < 0){ return mainExit(Err,"main(I): putString: <%s>"); }
   }
 
   while(fgets(resuPath,PATH_LEN,D))
   {
     Cnt = strlen(resuPath);
     resuPath[Cnt-1] = '\0';
-    F1 = strstr(resuPath,"asmlib.DefaultType(\"");
+    F1 = strstr(resuPath,MATCH_START_NEW_TYPE);
     F2 = strstr(resuPath,argv[4]);
-    if(!F && F1 != NULL && F2 != NULL){ F = 1; }
-    if( F && F1 != NULL && F2 == NULL){ F = 0; }
+    F3 = strstr(resuPath,"end"); // Closing the IF statement in LUA
+    if(!F &&  F1 != NULL &&  F2 != NULL){ F = 1; }
+    if( F && (F1 != NULL || (F3 != NULL && Cnt == 4)) &&  F2 == NULL ){ F = 0; }
     if(F)
     {
-      S = strstr(resuPath,"\"models/");
+      S = strstr(resuPath,MATCH_MODEL_STR);
       if(S != NULL)
       {
         S++;
-        E = strstr(S,".mdl\"");
+        E = strstr(S,MATCH_MODEL_END);
         if(E != NULL)
         {
           Len = (E - S + 4);
@@ -121,19 +146,21 @@ int main(int argc, char **argv)
           pathToGmod(resuPath);
           if(DataBase.findStringID(0,resuPath) == SSTACK_NOT_FOUND)
           {
-            DataBase.putString(resuPath);
+            Err = DataBase.putString(resuPath);
+            if(Err < 0){ return mainExit(Err,"main(D): putString: <%s>"); }
           }
         }
       }
     }
   }
 
-  DataBase.updateStack(&Addon,DA);
-  Addon.updateStack(&DataBase,AD);
+  fprintf(AD,"%s\n\n","These models were removed by the extension creator/owner.");
+  Err = Addon.printMismatch(&DataBase,AD);
+  if(Err < 0){ return mainExit(Err,"main(AD): printMismatch: <%s>"); }
 
-  fclose(I);
-  fclose(D);
-  fclose(DA);
-  fclose(AD);
-  return 0;
+  fprintf(DA,"%s\n\n","These models are missing in the database. Insert them.");
+  Err = DataBase.printMismatch(&Addon,DA);
+  if(Err < 0){ return mainExit(Err,"main(DA): printMismatch: <%s>"); }
+
+  return mainExit(0,"");
 }
--- peaces_manager/string_stack.h
+++ peaces_manager/string_stack.h
@@ -1,124 +1,145 @@
-#define SSTACK_DEPTH 10000
-#define SSTACK_SUCCESS      0
-#define SSTACK_INVALID_ID  -1
-#define SSTACK_MALLOC_FAIL -2
-#define SSTACK_NOT_FOUND   -3
-#define SSTACK_INVALID_OP  -4
-#define SSTACK_INV_POINTER NULL
+#ifndef __STRING_STACK_H_
+  #define __STRING_STACK_H_
 
-typedef struct st_string
-{
-  int   Len;
-  char *Data;
-} stString;
+  #define SSTACK_DEPTH 10000
+  #define SSTACK_SUCCESS      0
+  #define SSTACK_INVALID_ID  -1
+  #define SSTACK_MALLOC_FAIL -2
+  #define SSTACK_NOT_FOUND   -3
+  #define SSTACK_INVALID_OP  -4
+  #define SSTACK_INV_POINTER NULL
 
-typedef class string_stack
-{
-    private:
-      int Count;
-      int Log;
-stString *Stack[SSTACK_DEPTH];
-    public:
-        int getCount(void){ return Count; }
-        int putString(const char *strData);
-  stString *getString(int iID);
-        int findStringID(int iID, const char * strData);
-        int updateStack(struct string_stack *Other, FILE *File);
-      void      print(int iID);
-             string_stack(void);
-            ~string_stack(void);
-
-} cstrStack;
-
-string_stack::string_stack(void)
-{
-  Count = 0;
-}
-
-string_stack::~string_stack(void)
-{
-  int Item = 0;
-  while(Item < Count)
+  namespace sstack
   {
-    free(Stack[Item]->Data);
-    free(Stack[Item]);
-    Item++;
-  }
-}
-
-int string_stack::putString(const char *strData)
-{
-  if(Count >= SSTACK_DEPTH)
-  {
-    printf("putString: Stack overflow\n");
-    return SSTACK_INVALID_ID;
-  }
-  stString *String = (struct st_string*)malloc(sizeof(struct st_string));
-  if(SSTACK_INV_POINTER == String)
-  {
-    printf("putString: Failed to allocate memory for a string structure\n");
-    return SSTACK_MALLOC_FAIL;
-  }
-  String->Len  = strlen(strData);
-  String->Data = (char*)malloc((String->Len + 1) * sizeof(char));
-  if(SSTACK_INV_POINTER == String->Data)
-  {
-    printf("putString: Failed to allocate memory for a string\n");
-    return SSTACK_MALLOC_FAIL;
-  }
-  strcpy(String->Data,strData);
-  Stack[Count++] = String;
-  return (Count-1);
-}
-
-stString *string_stack::getString(int iID)
-{
-  if(iID >= 0 || iID < Count){ return Stack[iID]; }
-  printf("getString: No such ID #%d\n",iID ); return SSTACK_INV_POINTER;
-}
-
-void string_stack::print(int iID)
-{
-  if(!(iID >= 0 || iID < Count))
-    { printf("print: No such ID #%d\n",iID ); return; }
-  printf("[%u]<%s>\n",Stack[iID]->Len, Stack[iID]->Data);
-  return;
-}
-
-int string_stack::findStringID(int iID, const char *strData)
-{
-  if(SSTACK_INV_POINTER == strData)
-    { printf("findString: No data\n"); return -1; }
-  if(!(iID >= 0 || iID < Count))
-    { printf("findString: No such ID #%d\n",iID ); return SSTACK_INVALID_ID; }
-  while(iID < Count)
-  {
-    if(SSTACK_INV_POINTER != strstr(Stack[iID]->Data,strData))
-      { return iID; }
-    iID++;
-  }
-  return SSTACK_NOT_FOUND;
-}
-
-int string_stack::updateStack(struct string_stack *Other, FILE *File)
-{
-  if(SSTACK_INV_POINTER == Other){ printf("findString: No other stack\n"); return SSTACK_INVALID_OP; }
-  if(SSTACK_INV_POINTER == File){ printf("findString: No file\n"); return SSTACK_INVALID_OP; }
-  int oCount = Other->getCount();
-  int oItem  = 0, iStatus = 0;
-  stString *strOther;
-  while(oItem < oCount)
-  {
-    strOther = Other->getString(oItem);
-    if(NULL != strOther)
+    typedef struct st_string
     {
-      iStatus = findStringID(0,strOther->Data);
-      if(SSTACK_NOT_FOUND == iStatus)
+      int   Len;
+      char *Data;
+    } stString;
+
+    typedef class string_stack
+    {
+        private:
+          int Count;
+          int Log;
+    stString *Stack[SSTACK_DEPTH];
+        public:
+            int getCount(void){ return Count; }
+            int putString(const char *strData);
+      stString *getString(int iID);
+            int findStringID(int iID, const char * strData);
+            int printMismatch(struct string_stack *Other, FILE *File);
+          void      print(int iID);
+                 string_stack(void);
+                ~string_stack(void);
+
+    } cstrStack;
+
+    string_stack::string_stack(void)
+    {
+      Count = 0;
+    }
+
+    string_stack::~string_stack(void)
+    {
+      int Item = 0;
+      while(Item < Count)
       {
-        fprintf(File,"%s\n",strOther->Data);
+        free(Stack[Item]->Data);
+        free(Stack[Item]);
+        Item++;
       }
     }
-    oItem++;
+
+    int string_stack::putString(const char *strData)
+    {
+      if(Count >= SSTACK_DEPTH)
+      {
+        printf("putString: Stack overflow\n");
+        return SSTACK_INVALID_ID;
+      }
+      stString *String = (struct st_string*)malloc(sizeof(struct st_string));
+      if(SSTACK_INV_POINTER == String)
+      {
+        printf("putString: Failed to allocate memory for a string structure\n");
+        return SSTACK_MALLOC_FAIL;
+      }
+      String->Len  = strlen(strData);
+      String->Data = (char*)malloc((String->Len + 1) * sizeof(char));
+      if(SSTACK_INV_POINTER == String->Data)
+      {
+        printf("putString: Failed to allocate memory for a string\n");
+        return SSTACK_MALLOC_FAIL;
+      }
+      strcpy(String->Data,strData);
+      Stack[Count++] = String;
+      return (Count-1);
+    }
+
+    stString *string_stack::getString(int iID)
+    {
+      if(iID >= 0 || iID < Count){ return Stack[iID]; }
+      printf("getString: No such ID #%d\n",iID ); return SSTACK_INV_POINTER;
+    }
+
+    void string_stack::print(int iID)
+    {
+      if(!(iID >= 0 || iID < Count))
+        { printf("print: No such ID #%d\n",iID ); return; }
+      printf("[%u]=[%u]<%s>\n",iID,Stack[iID]->Len, Stack[iID]->Data);
+      return;
+    }
+
+    int string_stack::findStringID(int iID, const char *strData)
+    {
+      if(SSTACK_INV_POINTER == strData)
+        { printf("findString: No data\n"); return SSTACK_INVALID_ID; }
+      if(!(iID >= 0 || iID < Count))
+        { printf("findString: No such ID #%d\n",iID ); return SSTACK_INVALID_ID; }
+      while(iID < Count)
+      {
+        if(SSTACK_INV_POINTER != strstr(Stack[iID]->Data,strData))
+          { return iID; }
+        iID++;
+      }
+      return SSTACK_NOT_FOUND;
+    }
+
+    int string_stack::printMismatch(struct string_stack *Other, FILE *File)
+    {
+      if(SSTACK_INV_POINTER == Other)
+        { printf("printMismatch: Other stack missing\n"); return SSTACK_INVALID_OP; }
+      if(SSTACK_INV_POINTER == File){ File = stdout; }
+      int oCount = Other->getCount();
+      int oItem  = 0, iStatus = 0;
+      stString *strOther;
+      while(oItem < oCount)
+      {
+        strOther = Other->getString(oItem);
+        if(NULL != strOther)
+        {
+          iStatus = findStringID(0,strOther->Data);
+          if(SSTACK_NOT_FOUND == iStatus)
+          {
+            fprintf(File,"%s\n",strOther->Data);
+          }
+        }
+        oItem++;
+      }
+      return SSTACK_SUCCESS;
+    }
+
+    const char * const sstackErrMsg(int iErr)
+    {
+      switch(iErr)
+      {
+        case SSTACK_INVALID_ID : { return "Invalid stack ID"; }
+        case SSTACK_MALLOC_FAIL: { return "Memory allocation fail"; }
+        case SSTACK_NOT_FOUND  : { return "Item not found"; }
+        case SSTACK_INVALID_OP : { return "Invalid internal operation"; }
+      }
+      return "Success";
+    }
   }
-  return SSTACK_SUCCESS;
-}
+
+#endif
