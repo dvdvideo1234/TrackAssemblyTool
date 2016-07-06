f310e3a9b8a60515b9812d8750f2479fec28071f
Deyan Dobromirov <dvd_video@abv.bg>
2016-07-06 16:24:43 +0300

    Fixed: MakePiece logging when spawning out of map borders

--- lua/trackassembly/trackasmlib.lua
+++ lua/trackassembly/trackasmlib.lua
@@ -3135,7 +3135,7 @@ function MakePiece(pPly,sModel,vPos,aAng,nMass,sBgSkIDs,clColor,sMode)
   ePiece:SetNotSolid(false)
   ePiece:SetModel(sModel)
   if(not SetPosBound(ePiece,vPos or GetOpVar("VEC_ZERO"),pPly,sMode)) then
-    return StatusLog(nil,pPly:Nick().." spawned <"..sModel.."> outside of map bounds") end
+    return StatusLog(nil,"MakePiece: "..pPly:Nick().." spawned <"..sModel.."> outside of map bounds") end
   ePiece:SetAngles(aAng or GetOpVar("ANG_ZERO"))
   ePiece:Spawn()
   ePiece:Activate()
