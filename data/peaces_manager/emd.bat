9f00df3c3dbd41bf4df4456cdf5afb862499d9f6
Deyan Dobromirov <dvd_video@abv.bg>
2016-07-31 18:30:11 +0300

    Added: G gauge "c0512" turns and J-switchers Added: Category for G Gauge track pack Updated: Piece exporter batch now uses straight *.gma, instead doing this manually via the GMAD extractor by MulleDK19 Updated: E2 Export generator script using ", " as delimiter for *.lua exports

--- data/peaces_manager/emd.bat
+++ data/peaces_manager/emd.bat
@@ -1,15 +1,50 @@
-echo off
+@echo off
+setlocal enabledelayedexpansion
 
-set DataBase=F:\Games\Steam\steamapps\common\GarrysMod\garrysmod\addons\TrackAssemblyTool_GIT\lua\autorun\trackassembly_init.lua
-set ModelLst=models_list.txt
-set BasePath=%~dp0
+:: The location of the script ( Do not change )
+set emd_chew_pathb=%~dp0
+echo Running in: %emd_chew_pathb%
 
-del %ModelLst%
-del addon-db.txt
-del db-addon.txt
+:: The path to Addons
+set emd_chew_addon=F:\Games\Steam\steamapps\common\GarrysMod\garrysmod\addons
 
-dir /a-d /b /s *.mdl > %ModelLst%
+:: The path to the "trackassembly_init.lua"
+set emd_chew_dbase=%emd_chew_addon%\TrackAssemblyTool_GIT\lua\autorun\trackassembly_init.lua
 
-call chewpath.exe %BasePath% %ModelLst% %DataBase%
+:: Output file for models list
+set emd_chew_modls=models_list.txt
+
+:: Location of "gmad.exe"
+set emd_chew_binloc=F:\Games\Steam\steamapps\common\GarrysMod\bin
+
+:: How many addons are to be processed
+set emd_chew_addcnt=2
+
+:: GMA addtons to be processed
+set emd_chew_addlst[1]=rons_2ft_trackpack_634000136
+set emd_chew_addlst[2]=g_scale_track_pack_718239260
+
+:: Folder list for extraction and the directories they will be extracted
+set emd_chew_adddir[1]=Ron's 2ft track pack
+set emd_chew_adddir[2]=G Scale Track Pack
+
+:: Refresh model report
+del %emd_chew_pathb%%emd_chew_modls%
+
+:: Extract the GMA addons in the matching folders
+for /L %%k in (1,1,%emd_chew_addcnt%) do (
+  cd %emd_chew_pathb%
+  rd /S /Q "!emd_chew_adddir[%%k]!"
+  call %emd_chew_binloc%\gmad.exe extract -file "%emd_chew_addon%\!emd_chew_addlst[%%k]!.gma" -out "%emd_chew_pathb%!emd_chew_adddir[%%k]!"
+  cd "%emd_chew_pathb%!emd_chew_adddir[%%k]!"
+  dir /a-d /b /s *.mdl >> %emd_chew_pathb%%emd_chew_modls%
+)
+
+:: Refresh output files
+del %emd_chew_pathb%addon-db.txt
+del %emd_chew_pathb%db-addon.txt
+
+:: Chewing the paths uses base path relative to the executable
+call %emd_chew_pathb%chewpath.exe %emd_chew_pathb% %emd_chew_modls% %emd_chew_dbase%
 
 timeout 300
