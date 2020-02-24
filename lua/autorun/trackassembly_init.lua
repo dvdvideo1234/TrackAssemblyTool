9cc2e219cd1ad6a57677482c082090fa826fe853
Deyan Dobromirov <dvd_video@abv.bg>
2020-02-24 23:43:27 +0200

    Fixed: Integrate repo name with the addon name

--- lua/autorun/trackassembly_init.lua
+++ lua/autorun/trackassembly_init.lua
@@ -73,7 +73,7 @@ local gtInitLogs = {"*Init", false, 0}
 
 ------ CONFIGURE ASMLIB ------
 asmlib.InitBase("track","assembly")
-asmlib.SetOpVar("TOOL_VERSION","7.598")
+asmlib.SetOpVar("TOOL_VERSION","7.599")
 asmlib.SetIndexes("V" ,    "x",  "y",   "z")
 asmlib.SetIndexes("A" ,"pitch","yaw","roll")
 asmlib.SetIndexes("WV",1,2,3)
