1abf293af2c8872da06afe1a2dded6b8f4e09dc8
Deyan Dobromirov <dvd_video@abv.bg>
2020-04-10 19:04:54 +0300

    Added: External DSV manipulation menu translated to Japanese by Petar Dimov

--- lua/autorun/trackassembly_init.lua
+++ lua/autorun/trackassembly_init.lua
@@ -74,7 +74,7 @@ local gtInitLogs = {"*Init", false, 0}
 
 ------ CONFIGURE ASMLIB ------
 asmlib.InitBase("track","assembly")
-asmlib.SetOpVar("TOOL_VERSION","7.602")
+asmlib.SetOpVar("TOOL_VERSION","7.603")
 asmlib.SetIndexes("V" ,    "x",  "y",   "z")
 asmlib.SetIndexes("A" ,"pitch","yaw","roll")
 asmlib.SetIndexes("WV",1,2,3)
