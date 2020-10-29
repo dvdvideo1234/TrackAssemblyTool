7f1b44757be85fb78a8652ca461ac233e1a5585f
Deyan Dobromirov <dvd_video@abv.bg>
2020-10-29 22:14:38 +0200

    Change the naming of some controls

--- lua/autorun/trackassembly_init.lua
+++ lua/autorun/trackassembly_init.lua
@@ -87,7 +87,7 @@ local gtInitLogs = {"*Init", false, 0}
 ------------ CONFIGURE ASMLIB ------------
 
 asmlib.InitBase("track","assembly")
-asmlib.SetOpVar("TOOL_VERSION","7.674")
+asmlib.SetOpVar("TOOL_VERSION","7.675")
 asmlib.SetIndexes("V" ,    "x",  "y",   "z")
 asmlib.SetIndexes("A" ,"pitch","yaw","roll")
 asmlib.SetIndexes("WV",1,2,3)
