8f1ba33798191d25159c931098fdd87e5e3f20b3
dvdvideo1234 <dvd_video@abv.bg>
2021-01-05 12:40:43 +0200

    Fixed: Label for `material type` missing in french Fixed: Commit hook not showing correct Lua path in the log Reorganize: Translation hashes for `en` and `fr`

--- data/hooks/pre-commit.bat
+++ data/hooks/pre-commit.bat
@@ -8,7 +8,7 @@ set FrmEnds=UNX
 echo Base: %BasPath%>  %LogFile%
 echo Lua : %LuaPath%>> %LogFile%
 copy %CpyPath%\%OutFile% %CpyPath%\%InpFile%
-echo Calling %LuaPath%lua.exe>>%LogFile%
+echo Calling %LuaPath%\lua.exe>>%LogFile%
 echo Call[0]: %BasPath%pre-commit.lua>>%LogFile%
 echo Call[1]: %CpyPath%>>%LogFile%
 echo Call[2]: %OutFile%>>%LogFile%
@@ -20,4 +20,5 @@ echo Cleanup pieces manager buid...>>%LogFile%
 rd /s /q %BasPath%..\..\data\peaces_manager\bin
 rd /s /q %BasPath%..\..\data\peaces_manager\obj
 del %BasPath%..\..\data\peaces_manager\peaces_manager.layout
+timeout 500
 exit 0
