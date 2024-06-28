b51024132b9163f3f4e7795ff79e6e3ab7b72758
Deyan Dobromirov <dvd_video@abv.bg>
2024-06-28 15:41:20 +0300

    Fixed: E2 comment typos

--- lua/autorun/trackassembly_init.lua
+++ lua/autorun/trackassembly_init.lua
@@ -86,7 +86,7 @@ local asmlib = trackasmlib; if(not asmlib) then -- Module present
 ------------ CONFIGURE ASMLIB ------------
 
 asmlib.InitBase("track","assembly")
-asmlib.SetOpVar("TOOL_VERSION","8.780")
+asmlib.SetOpVar("TOOL_VERSION","8.781")
 
 ------------ CONFIGURE GLOBAL INIT OPVARS ------------
 
