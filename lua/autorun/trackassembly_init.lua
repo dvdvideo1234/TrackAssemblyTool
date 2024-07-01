35b7723b7c67693a8c1595ee4d01dd09013e68e5
dvdvideo1234 <dvd_video@abv.bg>
2024-07-01 14:40:28 +0300

    Added: Chinese translation

--- lua/autorun/trackassembly_init.lua
+++ lua/autorun/trackassembly_init.lua
@@ -86,7 +86,7 @@ local asmlib = trackasmlib; if(not asmlib) then -- Module present
 ------------ CONFIGURE ASMLIB ------------
 
 asmlib.InitBase("track","assembly")
-asmlib.SetOpVar("TOOL_VERSION","8.784")
+asmlib.SetOpVar("TOOL_VERSION","8.785")
 
 ------------ CONFIGURE GLOBAL INIT OPVARS ------------
 
